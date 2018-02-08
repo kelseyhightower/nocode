0a6d91ef8d32af69229c8beb36cf0c8a214c6642
Gary Sheppard <gsheppard@esri.com>
2018-02-08 16:41:22 -0500

    Added Java support
    - All the libraries needed to call No Code from a Java program
    - A sample app that shows how it's done

---
+++ Java support/README.md
@@ -0,0 +1,6 @@
+# Integrating Java with No Code
+
+It's very simple to call No Code from Java. This directory contains the following:
+
+- All the libraries needed to access No Code from Java
+- A sample Java app that demonstrates how these libraries are used
---
+++ Java support/java-no-code-sample/src/main/java/com/github/kelseyhightower/nocode/JavaNoCodeSampleApp.java
@@ -0,0 +1,13 @@
+package com.github.kelseyhightower.nocode;
+
+/**
+ * A sample app that shows how to integrate Java with No Code.
+ */
+public class JavaNoCodeSampleApp {
+  
+  public static void main(String[] args) {
+    // Here's where you call the No Code libraries:
+    
+  }
+  
+}
